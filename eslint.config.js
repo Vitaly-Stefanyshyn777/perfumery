b56c0bcdfc15bcae97e92206278cb4f6662b75e9
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2024-11-03 13:29:08 +0200

    ччччч

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,14 @@ export default {
     browser: true,
     es2021: true,
   },
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"], // Додайте 'plugin:vue/vue3-recommended' якщо використовуєте Vue
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
   rules: {
     // Додайте або змініть правила тут
+    // Наприклад, вимкнути правило semi
+    semi: ["error", "always"],
   },
 };
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  // Опції конфігурації можуть бути додані тут
+  // Задайте базовий шлях залежно від вашого розміщення
+  base: "/perfumery/", // Замініть на назву вашого репозиторію, якщо деплоїте на GitHub Pages піддомен
+  build: {
+    outDir: "dist",
+    // Додаткові налаштування збірки можуть бути додані тут
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 });
